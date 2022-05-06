5c9a2c2a8aa245bd12aaa3523cb4b0e9584fd560
Daniela Farias <farias.danicris@gmail.com>
2022-05-05 22:50:14 -0300

    Função de adicionar produto na home; Somar os produtos de mesmo ID do cart;

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { createContext, ReactNode, useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -29,10 +29,30 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     //   return JSON.parse(storagedCart);
     // }
 
-    return [];
+    return [
+      {
+        id: 1,
+        title: "Tênis de Caminhada Leve Confortável",
+        price: 179.9,
+        image:
+          "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg",
+        priceFormatted: "R$ 179,90",
+        amount: 2,
+      },
+      {
+        id: 2,
+        title: "Tênis VR Caminhada Confortável Detalhes Couro Masculino",
+        price: 139.9,
+        image:
+          "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg",
+        priceFormatted: "R$ 139,90",
+        amount: 3,
+      },
+    ];
   });
 
   const addProduct = async (productId: number) => {
+    console.log(productId);
     try {
       // TODO
     } catch {
